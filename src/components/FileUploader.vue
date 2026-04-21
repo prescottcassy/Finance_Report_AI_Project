bf6444535eec1473bfb7b4e24963af29b756fb95
Cassy Prescott <prescottcassy@gmail.com>
2026-04-21 14:12:38 -0500

    Add FileUploader component for uploading 10k PDF files

---
+++ src/components/FileUploader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div 
+        class="uploader"
+        @dragover.prevent
+        @drop.prevent="onDrop"
+    >
+        <input
+            type="file"
+            accept=".pdf"
+            ref="fileInput"
+            @change="onFileChange"
+            hidden
+        />
+    <div @click="fileInput.click()">
+        <p v-if="!fileName">Click or drag a 10k PDF file here </p>
+        <p v-else>{{ fileName }}</p>
+    </div>
+</template>
